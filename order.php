516416a5106a8a92344dcba2a291ce9b4e38016b
taviann <j.taviann@gmail.com>
2021-04-24 11:17:13 -0400

    Update order.php

--- order.php
+++ order.php
@@ -198,11 +198,10 @@ class foo{
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Restaurant Website</title>
     
-<!--    
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.js"></script>
--->
+
    
     <link rel="stylesheet" href="css/style.css">
 </head>
